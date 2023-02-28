03ae56023bf8ddf0d7a6a610572229ee3636d947
tlrowitz <tyarowitz@gmail.com>
2023-02-28 16:21:40 -0700

    DP Logo

--- responsive-architects/js/scripts.js
+++ responsive-architects/js/scripts.js
@@ -4,3 +4,20 @@ $('.menu-button').click(function(){
 
 // Initialize Animate before Scroll
 AOS.init();
+
+//Initialize jarallax
+
+$('.jarallax').jarallax({
+    speed: 0.5,
+    type: 'scale',
+  });
+
+  //Initialize rellax
+  var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
